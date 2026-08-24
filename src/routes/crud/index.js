6c6f0e528bb75a13c9b782e5936a7b5b253328ed
direccion001 <direccion@inglesporresultados.com>
2026-08-24 08:37:39 -0600

    Montar CRUD de detalle de asistencias

--- src/routes/crud/index.js
+++ src/routes/crud/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
 const prospectosRouter = require("./prospectos");
+const detalleAsistenciasRouter = require("./detalle-asistencias");
 
 const router = express.Router();
 
 router.use("/prospectos", prospectosRouter);
+router.use("/detalle-asistencias", detalleAsistenciasRouter);
 
 module.exports = router;
