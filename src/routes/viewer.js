9f824321f284fd4597e1bc40bf2abde4567f97d0
direccion001 <direccion@inglesporresultados.com>
2026-08-24 14:29:48 -0600

    Corregir template SQL de contactos

--- src/routes/viewer.js
+++ src/routes/viewer.js
@@ -247,7 +247,7 @@ router.get("/prospectos/:id_evaluacion/contactos", async (req, res) => {
         ON e.id_evaluacion = c.id_evaluacion
 
       LEFT JOIN USUARIOS u
-        ON u.`ID Usuario` = c.id_usuario
+        ON u.\`ID Usuario\` = c.id_usuario
 
       WHERE c.id_evaluacion = ?
     `;
