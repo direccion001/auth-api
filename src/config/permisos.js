ca6022c07c49083d32adbf84743d7de8388abc25
direccion001 <direccion@inglesporresultados.com>
2026-08-24 12:27:13 -0600

    Habilitar módulo de seguimientos para internos

--- src/config/permisos.js
+++ src/config/permisos.js
@@ -9,6 +9,7 @@ module.exports = {
     "asistencias",
     "calificaciones",
     "prospectos",
+    "seguimientos",
     "dashboard"
   ],
 
@@ -16,6 +17,7 @@ module.exports = {
     "asistencias",
     "calificaciones",
     "prospectos",
+    "seguimientos",
     "dashboard"
   ]
 };
